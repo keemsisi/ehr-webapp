4538d42c4db801143944c8812345b0cf91894044
keemsisi <keemsisi@gmail.com>
2021-07-08 20:06:25 +0100

    updating the login page

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CustomHttpClientService } from '../Services/custom-http-client.service';
 
 
 @NgModule({
@@ -15,6 +16,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     LoginRoutingModule,
     ReactiveFormsModule,
     FormsModule
-  ]
+  ],
+  providers: [CustomHttpClientService]
 })
 export class LoginModule { }
